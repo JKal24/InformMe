bb0316f3ea775d33928a4df67e684468fd1c7975
JKal24 <jkalirai2499@gmail.com>
2022-01-01 02:21:50 -0500

    Adding recycle view and adapter functionality to create news feed

---
+++ app/src/main/java/com/astro/informme/api/News.kt
@@ -0,0 +1,4 @@
+package com.astro.informme.api
+
+class News(val title : String, val name : String, val description : String, val url : String, val image : String) {
+}
--- app/src/main/java/com/astro/informme/api/Trends.kt
+++ app/src/main/java/com/astro/informme/api/Trends.kt
@@ -7,28 +7,24 @@ import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.astro.informme.BuildConfig
+import com.astro.informme.NewsFragment
 
 class Trends {
 
     fun getTrends(context: Context, country : String) {
 
         val queue = Volley.newRequestQueue(context)
-        val baseTrendsUrl = "https://d236-72-53-195-163.ngrok.io/trends/canada"
+        val baseTrendsUrl = "https://d236-72-53-195-163.ngrok.io/trends/$country"
 
         val jsonArrayRequest = JsonArrayRequest(baseTrendsUrl, { response ->
-            var count = 1;
 
-            for (i in 0..response.length()) {
-                if (count > 3) break
+            for (i in 0 until response.length()) {
                 val keyword = java.net.URLEncoder.encode(response.get(i) as String?, "utf-8")
 
                 fetchNews(queue, keyword)
-
-                count++
             }
 
         }, {
-            print("not found")
         })
 
         queue.add(jsonArrayRequest)
@@ -36,7 +32,7 @@ class Trends {
     }
 
     private fun fetchNews(queue : RequestQueue, keyword : String) {
-        val url = "https://newsapi.org/v2/everything?q=" + keyword + "&from=2021-12-31&pageSize=2&sortBy=popularity&apiKey=" + BuildConfig.key
+        val url = "https://newsapi.org/v2/everything?q=" + keyword + "&from=2021-12-31&pageSize=1&sortBy=popularity&apiKey=" + BuildConfig.key
         //making a request
         val jsonObjectRequest = object: JsonObjectRequest(
             Method.GET,
@@ -44,17 +40,18 @@ class Trends {
             null,
             Response.Listener {
                 val newsJsonArray = it.getJSONArray("articles")
-                val newsArray = ArrayList<News>()
+
                 for(i in 0 until newsJsonArray.length()) {
                     val newsJsonObject = newsJsonArray.getJSONObject(i)
                     val news = News(
                         newsJsonObject.getString("title"),
-                        newsJsonObject.getString("name"),
-                        newsJsonObject.getString("author"),
+                        newsJsonObject.getJSONObject("source").getString("name"),
+                        newsJsonObject.getString("description"),
                         newsJsonObject.getString("url"),
                         newsJsonObject.getString("urlToImage")
                     )
-                    newsArray.add(news)
+
+                    NewsFragment().updateList(news)
                 }
 
             },
@@ -71,10 +68,6 @@ class Trends {
         }
 
         queue.add(jsonObjectRequest)
-    }
-
-    class News(val title : String, val name : String, val author : String, val url : String, val image : String) {
-
     }
 
 }
